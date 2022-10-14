be207ff748c805ea2ff89986db30a53798968e5a
FloresVera <63812827+FloresVera@users.noreply.github.com>
2022-10-14 16:06:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const textos = [
-  'probar',
   'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
   'There is nothing more deceptive than an obvious fact.',
   'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
